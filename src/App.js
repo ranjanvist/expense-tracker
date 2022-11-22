dc29bce35bc6ac33e112d2cdb9e16e3564c157e1
Ranjan Mishra <ranjanvist@gmail.com>
2022-11-22 18:21:23 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import ExpenseItem from './components/expense-items';
+
+function App() {
+  return (
+    <div className="App">
+      react app
+      <ExpenseItem />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/expense-items/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const ExpenseItem = () =>{
+    return (
+        <div>
+            <h2>Expense Items</h2>
+        </div>
+    )
+}
+export default ExpenseItem;
